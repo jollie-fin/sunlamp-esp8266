d030a1128dd9f9e3a7b2104ca8287b4152bf3006
jollie-fin <1067794+jollie-fin@users.noreply.github.com>
2024-02-29 05:47:13 +0100

    Upgrading README

--- README.md
+++ README.md
@@ -1,7 +1,29 @@
 # sunlamp-esp8266
 
- LED-ribbons on aluminium profiles, driven by an ESP8266
+A bunch of LED-ribbons on aluminium profiles, driven by an ESP8266
 
-Finland is famously dark in winter, so I wanted to have a big lamp with natural white, that could fill up a living room with nice warm light. Color temperature can be changed through wifi. Automatic power up and down still has to be done.
+Finland is famously dark in winter, so I wanted to have a big lamp with natural white, that could fill up a living room with nice warm light.
+
+Designed around an ESP8266 on a protoboard, some mosfet drivers, two mosfets. Probably not ok EMI-wise...
+
+A wifi interface is available to change color temperature and brightness
+
+Some cares have been taken to reduce standby power consumption (<1W)
+
+Features a few on-off events stored in emulated-EEPROM, to be used as an alarm-clock
 
 Thanks to [DIY Perks](https://youtu.be/V5uycGosYq4?t=491) for the idea
+
+## Issues
+
+Designing time-sensitive things with an ESP8266 brings a few issues :
+
+- PWM is software-emulated, meaning it's slow. It's slightly "vibrating", which can lead to some headaches
+- PWM stops when Wifi communicates
+
+An RPI Pico W would have been a better choice, since it features hardware PWN
+
+## Images
+
+![Circuit board](./images%20for%20README/lamp_4.jpg 'Circuit board')
+![Main view](./images%20for%20README/lamp_2.jpg 'Main view')
--- lamp.ino
+++ lamp.ino
@@ -39,7 +39,7 @@ typedef uint16_t linear_light_t;
 #define FREE_TRANSITION_SPEED 5000
 
 #define SECONDS_PER_DAY 86400
-#define EPOCH_DAY 3 //1 january 1970 was a thursday. 0 is monday
+#define EPOCH_DAY 3 // 1 january 1970 was a thursday. 0 is monday
 #define EVENTS_COUNT 6
 
 #define LIGHT_SHAPE_COUNT 4
@@ -60,28 +60,27 @@ struct __attribute__((packed)) event_t
   char name[9];
 };
 
-
 ESP8266WebServer server(80);
-
-
 
 template <size_t BUFFER_SIZE>
 class SuperSerial : public Print
 {
-  public:
+public:
   using Print::Print;
-  size_t write(const uint8_t *buffer, size_t size) override;   // Overriding base functionality
+  size_t write(const uint8_t *buffer, size_t size) override; // Overriding base functionality
   size_t write(uint8_t) override;
 
   void send();
-  protected:
-    char buffer_print[BUFFER_SIZE];
-    char *buffer_ptr = buffer_print;
+
+protected:
+  char buffer_print[BUFFER_SIZE];
+  char *buffer_ptr = buffer_print;
 };
 
 SuperSerial<4096> superSerial;
 
-linear_light_t userToLin(user_light_t light) {
+linear_light_t userToLin(user_light_t light)
+{
   int32_t retval = light;
   retval *= LINEAR_RANGE;
   retval /= MAX_USER_LIGHT;
@@ -90,12 +89,13 @@ linear_light_t userToLin(user_light_t light) {
   return retval;
 }
 
-user_light_t linToUser(linear_light_t light) {
+user_light_t linToUser(linear_light_t light)
+{
   int32_t retval = light;
   retval *= MAX_USER_LIGHT;
   retval /= LINEAR_RANGE;
   retval = std::min(retval, MAX_USER_LIGHT);
-//  superSerial.printf("linToUser(%" PRIu16 ") = %" PRIi32 "\n", light, retval);
+  //  superSerial.printf("linToUser(%" PRIu16 ") = %" PRIi32 "\n", light, retval);
   return retval;
 }
 
@@ -103,14 +103,11 @@ user_light_t linToUser(linear_light_t light) {
 #define UPDATE_EEPROM_MS (10 * MS_IN_MIN)
 
 // Replace with your network credentials
-const char* ssid_ap     = "speedomobile";
-const char* password_ap = "***REMOVED***";
+const char *ssid_ap = "speedomobile";
+const char *password_ap = "***REMOVED***";
 
-const char *ssid     = "***REMOVED***";
-const char *password = ""***REMOVED***";
-
-/*const char *ssid     = "***REMOVED***";
-const char *password = "***REMOVED***";*/
+const char *ssid = "***REMOVED***";
+const char *password = " ***REMOVED *** ";
 
 const char *lamp_name = "sunlamp";
 
@@ -122,14 +119,14 @@ void decode_data(char *data);
 
 void handle_switch();
 
-linear_light_t  get_light_goal();
+linear_light_t get_light_goal();
 void transition_to(user_light_t next_light, uint32_t transition_ms);
 void transition_with_shape(light_datapoint_t light_shape[], int light_shape_count);
 void update_light();
 bool is_in_transition();
 bool is_millis_increasing(uint32_t before, uint32_t after);
-linear_light_t  get_light_level();
-void set_light_level(linear_light_t  light);
+linear_light_t get_light_level();
+void set_light_level(linear_light_t light);
 
 void init_eeprom();
 void setup_eeprom();
@@ -454,11 +451,14 @@ async function updateEvents() {
 
 ////////////// Websocket:send_message ///////////////////
 
-auto boolToStr(bool bla) {
+auto boolToStr(bool bla)
+{
   return bla ? F("true") : F("false");
 }
-void ajaxEvent(int i) {
-  if (i >= EVENTS_COUNT || i < 0) {
+void ajaxEvent(int i)
+{
+  if (i >= EVENTS_COUNT || i < 0)
+  {
     server.send(404, "text/json", "\"unknown event\"");
     return;
   }
@@ -469,35 +469,45 @@ void ajaxEvent(int i) {
   snprintf(buffer_name, sizeof(buffer_name), "%s", event.name);
 
   snprintf_P(
-    str,
-    sizeof(str),
-    PSTR("{\"id\":%d,\"days\":[%s,%s,%s,%s,%s,%s,%s],\"time\":%" PRIu32 ",\"ramp_in\":%d,\"hold\":%d,\"ramp_out\":%d,\"power\":%d,\"name\":\"%s\"}"),
-    i,
-    boolToStr(event.days & (1u<<0)),
-    boolToStr(event.days & (1u<<1)),
-    boolToStr(event.days & (1u<<2)),
-    boolToStr(event.days & (1u<<3)),
-    boolToStr(event.days & (1u<<4)),
-    boolToStr(event.days & (1u<<5)),
-    boolToStr(event.days & (1u<<6)),
-    event.time,
-    static_cast<int>(event.ramp_in),
-    static_cast<int>(event.hold),
-    static_cast<int>(event.ramp_out),
-    static_cast<int>(event.power),
-    buffer_name);
+      str,
+      sizeof(str),
+      PSTR("{\"id\":%d,\"days\":[%s,%s,%s,%s,%s,%s,%s],\"time\":%" PRIu32 ",\"ramp_in\":%d,\"hold\":%d,\"ramp_out\":%d,\"power\":%d,\"name\":\"%s\"}"),
+      i,
+      boolToStr(event.days & (1u << 0)),
+      boolToStr(event.days & (1u << 1)),
+      boolToStr(event.days & (1u << 2)),
+      boolToStr(event.days & (1u << 3)),
+      boolToStr(event.days & (1u << 4)),
+      boolToStr(event.days & (1u << 5)),
+      boolToStr(event.days & (1u << 6)),
+      event.time,
+      static_cast<int>(event.ramp_in),
+      static_cast<int>(event.hold),
+      static_cast<int>(event.ramp_out),
+      static_cast<int>(event.power),
+      buffer_name);
   server.send(200, "text/json", str);
 }
-const char *get_weekday() {
-  switch(timeClient.getDay()) {
-    case 1: return PSTR("Monday");
-    case 2: return PSTR("Tuesday");
-    case 3: return PSTR("Wednesday");
-    case 4: return PSTR("Thursday");
-    case 5: return PSTR("Friday");
-    case 6: return PSTR("Saturday");
-    case 0: return PSTR("Sunday");
-    default:return PSTR("");
+const char *get_weekday()
+{
+  switch (timeClient.getDay())
+  {
+  case 1:
+    return PSTR("Monday");
+  case 2:
+    return PSTR("Tuesday");
+  case 3:
+    return PSTR("Wednesday");
+  case 4:
+    return PSTR("Thursday");
+  case 5:
+    return PSTR("Friday");
+  case 6:
+    return PSTR("Saturday");
+  case 0:
+    return PSTR("Sunday");
+  default:
+    return PSTR("");
   }
 }
 void ajaxPower()
@@ -530,7 +540,6 @@ int32_t parse_int(const String &data, int bit_size)
   return std::min(std::max(retval, -(1 << (bit_size - 1))), (1 << (bit_size - 1)) - 1);
 }
 
-
 void handleRoot()
 {
   server.send_P(200, "text/html", index_html);
@@ -542,43 +551,65 @@ void handleAjax()
   event_t event = {};
   int nb_fields = 0;
 
-  for (int i = 0; i < server.args(); i++) {
+  for (int i = 0; i < server.args(); i++)
+  {
     const String &key = server.argName(i);
     const String &data = server.arg(i);
-    if (key == F("events_count")) {
+    if (key == F("events_count"))
+    {
       server.send(200, "text/json", String(EVENTS_COUNT));
       return;
-    } else if (key == F("get_current")) {
+    }
+    else if (key == F("get_current"))
+    {
       ajaxPower();
       return;
-    } else if (key.startsWith(F("get_event"))) {
+    }
+    else if (key.startsWith(F("get_event")))
+    {
       ajaxEvent(data.toInt());
       return;
-    } else if (key == F("id")) {
+    }
+    else if (key == F("id"))
+    {
       id = data.toInt();
       nb_fields++;
-    } else if (key == F("time")) {
-      event.time = parse_uint(data,17);
+    }
+    else if (key == F("time"))
+    {
+      event.time = parse_uint(data, 17);
       nb_fields++;
-    } else if (key == F("ramp_in")) {
-      event.ramp_in = parse_uint(data,8);
+    }
+    else if (key == F("ramp_in"))
+    {
+      event.ramp_in = parse_uint(data, 8);
       nb_fields++;
-    } else if (key == F("ramp_out")) {
-      event.ramp_out = parse_uint(data,8);
+    }
+    else if (key == F("ramp_out"))
+    {
+      event.ramp_out = parse_uint(data, 8);
       nb_fields++;
-    } else if (key == F("hold")) {
-      event.hold = parse_uint(data,8);
+    }
+    else if (key == F("hold"))
+    {
+      event.hold = parse_uint(data, 8);
       nb_fields++;
-    } else if (key == F("power")) {
-      event.power = parse_uint(data,8);
+    }
+    else if (key == F("power"))
+    {
+      event.power = parse_uint(data, 8);
       nb_fields++;
-    } else if (key == F("name")) {
+    }
+    else if (key == F("name"))
+    {
       char *dst = event.name;
-      for (unsigned i = 0; i < sizeof(event.name); i++) {
+      for (unsigned i = 0; i < sizeof(event.name); i++)
+      {
         char c = data[i];
         if (!c)
           break;
-        if (!isalpha(c) && !isdigit(c) && c != '.' && c != ' ' && c != '_') {
+        if (!isalpha(c) && !isdigit(c) && c != '.' && c != ' ' && c != '_')
+        {
           break;
         }
         *dst++ = c;
@@ -586,31 +617,41 @@ void handleAjax()
       if (static_cast<unsigned>(dst - event.name) < sizeof(event.name))
         *dst = '\0';
       nb_fields++;
-    } else if (key == F("days")) {
-      if (data.length() != 7) {
+    }
+    else if (key == F("days"))
+    {
+      if (data.length() != 7)
+      {
         superSerial.print(F("Impossible to parse days : "));
         superSerial.println(data);
         continue;
       }
-      for (int i = 0; i < 7; i++) {
+      for (int i = 0; i < 7; i++)
+      {
         event.days |= (!strchr(" .OoNnFf", data[i])) << i;
       }
       nb_fields++;
-    } else if (key == F("goal")) {
-      superSerial.printf("Power : %i\n", (int)parse_uint(data,8));
+    }
+    else if (key == F("goal"))
+    {
+      superSerial.printf("Power : %i\n", (int)parse_uint(data, 8));
 
-      transition_to(parse_uint(data,8), FREE_TRANSITION_SPEED);     
-    } else {
+      transition_to(parse_uint(data, 8), FREE_TRANSITION_SPEED);
+    }
+    else
+    {
       superSerial.print(F("Unrecognized key : "));
       superSerial.println(key);
       continue;
     }
   }
-  if (nb_fields) {
-    if (nb_fields != 8 || id < 0 || id >= EVENTS_COUNT) {
+  if (nb_fields)
+  {
+    if (nb_fields != 8 || id < 0 || id >= EVENTS_COUNT)
+    {
       superSerial.println(F("Impossible to decode event"));
       server.send(404, "text/json", "null");
-      return;    
+      return;
     }
     set_event(event, id);
   }
@@ -620,12 +661,12 @@ void handleAjax()
 
 /////////////////////////////// Setup ////////////////////////////////////////
 
-void setup() { 
+void setup()
+{
   delay(1000);
   Serial.begin(115200);
-  //setup_eeprom();
+  // setup_eeprom();
   init_eeprom();
-
 
   pinMode(SWITCH, INPUT_PULLUP);
   pinMode(HOT_LED, OUTPUT);
@@ -634,15 +675,13 @@ void setup() {
 
   superSerial.print(F("Event size :"));
   superSerial.println(sizeof(event_t));
-  
+
   // Connect to Wi-Fi network with SSID and password
   superSerial.println(F(""));
   superSerial.println(F("Connecting to "));
   superSerial.println(ssid);
   superSerial.println(password);
 
-
-  
   int max_trial = 50;
   superSerial.print("Trying for ");
   superSerial.print(max_trial / 2);
@@ -658,19 +697,20 @@ void setup() {
   }
   superSerial.println("");
 
-
   IPAddress myIP;
-  if (trial == max_trial) {
-      superSerial.println("Unable. Starting own access point");
-      IPAddress apIP(10, 10, 10, 10);
-      WiFi.mode(WIFI_AP);
-      WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
-      WiFi.softAP(ssid_ap, password_ap);
-      myIP = WiFi.softAPIP();
-  } else {
-      myIP = WiFi.localIP();           // Send the IP address of the ESP8266 to the computer
+  if (trial == max_trial)
+  {
+    superSerial.println("Unable. Starting own access point");
+    IPAddress apIP(10, 10, 10, 10);
+    WiFi.mode(WIFI_AP);
+    WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+    WiFi.softAP(ssid_ap, password_ap);
+    myIP = WiFi.softAPIP();
   }
-
+  else
+  {
+    myIP = WiFi.localIP(); // Send the IP address of the ESP8266 to the computer
+  }
 
   // Port defaults to 8266
   ArduinoOTA.setPort(8266);
@@ -682,7 +722,8 @@ void setup() {
   // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
   // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
 
-  ArduinoOTA.onStart([]() {
+  ArduinoOTA.onStart([]()
+                     {
     String type;
     if (ArduinoOTA.getCommand() == U_FLASH) {
       type = "sketch";
@@ -691,15 +732,13 @@ void setup() {
     }
 
     // NOTE: if updating FS this would be the place to unmount FS using FS.end()
-    superSerial.println("Start updating " + type);
-  });
-  ArduinoOTA.onEnd([]() {
-    superSerial.println(F("\nEnd"));
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    superSerial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
+    superSerial.println("Start updating " + type); });
+  ArduinoOTA.onEnd([]()
+                   { superSerial.println(F("\nEnd")); });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total)
+                        { superSerial.printf("Progress: %u%%\r", (progress / (total / 100))); });
+  ArduinoOTA.onError([](ota_error_t error)
+                     {
     superSerial.printf("Error[%u]: ", error);
     if (error == OTA_AUTH_ERROR) {
       superSerial.println(F("Auth Failed"));
@@ -711,8 +750,7 @@ void setup() {
       superSerial.println(F("Receive Failed"));
     } else if (error == OTA_END_ERROR) {
       superSerial.println(F("End Failed"));
-    }
-  });
+    } });
   ArduinoOTA.begin();
   /*
   if (!MDNS.begin(lamp_name)) {             // Start the mDNS responder for esp8266.local
@@ -725,24 +763,25 @@ void setup() {
 */
   server.on("/", handleRoot);
   server.on("/ajax", handleAjax);
-  server.on("/log", [](){superSerial.send();});
-  
+  server.on("/log", []()
+            { superSerial.send(); });
+
   server.begin();
 
   timeClient.begin();
-  timeClient.setTimeOffset(3*3600);
-
+  timeClient.setTimeOffset(3 * 3600);
 }
 
 ////////////////////////////// loop /////////////////////////////////
 
-void loop(){
+void loop()
+{
   server.handleClient();
   ArduinoOTA.handle();
 
   MDNS.update();
   update_light();
-//  update_eeprom();
+  //  update_eeprom();
   timeClient.update();
 
   handle_switch();
@@ -751,8 +790,9 @@ void loop(){
     return;
 
   const event_t *event = get_current_event(timeClient.getEpochTime());
-  if (!event) {
-    return;  
+  if (!event)
+  {
+    return;
   }
 
   superSerial.printf(PSTR("triggering event %s\n"), event->name);
@@ -777,11 +817,11 @@ struct eeprom_state_t
 } g_eeprom_state = {};
 
 bool g_eeprom_state_dirty = false;
- 
+
 void init_eeprom()
 {
-    g_eeprom_state.events[0] = event_t{.power = 60, .ramp_in = 20, .hold = 40, .ramp_out = 30, .time = 21*3600+10*60, .days = 0x7F, .name = "bisounou"};
-    g_eeprom_state.events[1] = event_t{.power = 200, .ramp_in = 20, .hold = 40, .ramp_out = 10, .time = 6 *3600+30*60, .days = 0x7F, .name = "cocorico"};
+  g_eeprom_state.events[0] = event_t{.power = 60, .ramp_in = 20, .hold = 40, .ramp_out = 30, .time = 21 * 3600 + 10 * 60, .days = 0x7F, .name = "bisounou"};
+  g_eeprom_state.events[1] = event_t{.power = 200, .ramp_in = 20, .hold = 40, .ramp_out = 10, .time = 6 * 3600 + 30 * 60, .days = 0x7F, .name = "cocorico"};
 }
 
 void setup_eeprom()
@@ -793,7 +833,8 @@ void setup_eeprom()
 void update_eeprom()
 {
   static uint32_t last_eeprom_update = 0;
-  if (millis() - last_eeprom_update > UPDATE_EEPROM_MS && g_eeprom_state_dirty) {
+  if (millis() - last_eeprom_update > UPDATE_EEPROM_MS && g_eeprom_state_dirty)
+  {
     EEPROM.put(0, g_eeprom_state);
     EEPROM.commit();
     last_eeprom_update = millis();
@@ -803,7 +844,8 @@ void update_eeprom()
 
 void set_event(const event_t &event, uint8_t no_event)
 {
-  if (no_event > EVENTS_COUNT) {
+  if (no_event > EVENTS_COUNT)
+  {
     return;
   }
   g_eeprom_state.events[no_event] = event;
@@ -819,15 +861,18 @@ const event_t *get_current_event(unsigned long current_epoch)
 {
   int day_of_week = ((current_epoch / SECONDS_PER_DAY) + EPOCH_DAY) % 7;
   uint32_t seconds_in_day = current_epoch % SECONDS_PER_DAY;
-  for (int i = 0; i < EVENTS_COUNT; i++) {
+  for (int i = 0; i < EVENTS_COUNT; i++)
+  {
     const event_t &event = g_eeprom_state.events[i];
-    if (!(event.days & (1 << day_of_week))) {
+    if (!(event.days & (1 << day_of_week)))
+    {
       continue;
     }
     uint32_t start_timestamp = event.time;
     uint32_t end_timestamp = start_timestamp + event.ramp_in * 60;
     uint32_t timestamp = seconds_in_day < start_timestamp ? seconds_in_day + SECONDS_PER_DAY : seconds_in_day;
-    if (timestamp > end_timestamp) {
+    if (timestamp > end_timestamp)
+    {
       continue;
     }
     return &event;
@@ -842,7 +887,7 @@ void handle_switch()
   static uint32_t last_switch_time = millis();
   static int32_t last_button_state = digitalRead(SWITCH);
   static int32_t button_state = last_button_state;
-  
+
   int32_t current_state = digitalRead(SWITCH);
   uint32_t switch_time = millis();
   if (current_state != last_button_state)
@@ -853,20 +898,23 @@ void handle_switch()
 
   if (switch_time - last_switch_time > DEBOUNCE_DELAY_MS)
   {
-    if (current_state != button_state) {
+    if (current_state != button_state)
+    {
       superSerial.println(current_state);
-      if (get_light_level()) {
+      if (get_light_level())
+      {
         superSerial.println("Goodnight");
         transition_to(0, FREE_TRANSITION_SPEED);
-      } else {
+      }
+      else
+      {
         superSerial.println("Raise and shine");
         transition_to(MAX_USER_LIGHT, FREE_TRANSITION_SPEED);
       }
-    }   
+    }
     button_state = current_state;
   }
 }
-
 
 //////////////// LIGHT LEVEL HANDLER ////////////////////
 
@@ -897,14 +945,16 @@ void transition_with_shape(light_datapoint_t light_shape[], int light_shape_coun
   g_light_shape[0].power = get_light_level();
 
   int i;
-  for (i = 0; i < light_shape_count; i++) {   
-    g_light_shape[i+1].timestamp = start + light_shape[i].offset;
+  for (i = 0; i < light_shape_count; i++)
+  {
+    g_light_shape[i + 1].timestamp = start + light_shape[i].offset;
 
-    g_light_shape[i+1].power = userToLin(light_shape[i].power);
+    g_light_shape[i + 1].power = userToLin(light_shape[i].power);
   }
   g_light_shape_count = light_shape_count;
 
-  for (int i = 0; i <= light_shape_count; i++) {
+  for (int i = 0; i <= light_shape_count; i++)
+  {
     superSerial.printf("shape %i at %" PRIu32 " = %" PRIu16 "\n", i, g_light_shape[i].timestamp, g_light_shape[i].power);
   }
 }
@@ -914,8 +964,8 @@ void update_light()
   uint32_t current = millis();
   for (int i = 0; i < g_light_shape_count; i++)
   {
-    if (is_millis_increasing(g_light_shape[i].timestamp, current)
-        && is_millis_increasing(current, g_light_shape[i + 1].timestamp)) {
+    if (is_millis_increasing(g_light_shape[i].timestamp, current) && is_millis_increasing(current, g_light_shape[i + 1].timestamp))
+    {
       uint64_t transition_start_time = g_light_shape[i].timestamp;
       uint64_t transition_end_time = g_light_shape[i + 1].timestamp;
       uint64_t transition_start_level = g_light_shape[i].power;
@@ -929,7 +979,8 @@ void update_light()
     }
   }
 
-  if (g_light_shape_count) {
+  if (g_light_shape_count)
+  {
     g_light_goal = g_light_shape[g_light_shape_count].power;
     set_light_level(g_light_shape[g_light_shape_count].power);
     superSerial.println(F("finished"));
@@ -957,7 +1008,7 @@ linear_light_t get_light_level()
 
 gamma_light_t gamma_law(linear_light_t light)
 {
-  return std::pow(light/static_cast<float>(LINEAR_RANGE), GAMMA_COEFF) * TOTAL_GAMMA_RANGE;
+  return std::pow(light / static_cast<float>(LINEAR_RANGE), GAMMA_COEFF) * TOTAL_GAMMA_RANGE;
 }
 
 void set_light_level(linear_light_t light)
@@ -965,13 +1016,18 @@ void set_light_level(linear_light_t light)
   if (light == g_current_light)
     return;
   gamma_light_t gamma = gamma_law(light);
-  if (gamma < HOT_RANGE) {
+  if (gamma < HOT_RANGE)
+  {
     analogWrite(HOT_LED, gamma / HOT_COEFF);
     analogWrite(COLD_LED, 0);
-  } else if (gamma < TOTAL_GAMMA_RANGE) {
+  }
+  else if (gamma < TOTAL_GAMMA_RANGE)
+  {
     analogWrite(HOT_LED, PWM_RANGE);
     analogWrite(COLD_LED, (gamma - HOT_RANGE) / COLD_COEFF);
-  } else {
+  }
+  else
+  {
     analogWrite(HOT_LED, PWM_RANGE);
     analogWrite(COLD_LED, PWM_RANGE);
   }
@@ -1004,6 +1060,3 @@ void SuperSerial<BUFFER_SIZE>::send()
   server.send(200, "application/octet-stream", buffer_print, buffer_ptr - buffer_print);
   buffer_ptr = buffer_print;
 }
-
-
-  
